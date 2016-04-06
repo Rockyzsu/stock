f81af1ff68e49a3a9a8509850cd16f3c314f1199
Rocky Chen <jinweizsu@gmail.com>
2016-04-06 16:45:36 +0800

    update time

--- tushare_case.py
+++ tushare_case.py
@@ -33,6 +33,7 @@ class StockBox:
         # print price_change[['time','change','type','volume']]
         big_share = ts.get_sina_dd(id, date=self.date)
         print big_share[['time', 'code', 'price', 'preprice', 'volume', 'type']]
+        print big_share.sort(columns='volume')
 
     def looper(self, id):
         while (1):
@@ -66,20 +67,51 @@ class StockBox:
         print dd[['name', 'nums', 'clast', 'amount']]
 
 
+    def date_store(self):
+        df=ts.get_hist_data("300333")
+        #df.to_csv("300333.cvs")
+        df.to_excel("300333.xlsx")
+
+    def profit_test(self):
+        df=ts.profit_data(top=60)
+        print df.sort_value(by='shares',ascending=False)
+
+    def longhubang(self):
+
+        df=ts.broker_tops(days=5)
+        df_save=df.sort_values(by='count',ascending=False)
+        df_save.to_excel("longhubang-10day.xlsx")
+
+
+
+    '''
+    Keep on this
+    '''
+    def daily_longhu(self):
+        #date=self.date
+        date='2016-04-05'
+        df = ts.top_list(date)
+        df.to_excel(date+".xlsx")
+
+
 def main():
     now = time.strftime("%Y-%m-%d")
-    print now
+    #print now
     token = '60517739976b768e07823056c6f9cb0fee33ed55a1709b3eaa14a76c6a1b7a56'
     sb = StockBox()
     # sb.looper(id)
-    # sb.realtime(id)
+    id='300333'
+    #sb.realtime(id)
     # stockBox.base_function("300333")
     # pandas_test=Pandas_test()
     # pandas_test.test_function()
     # sb.longhuban('2016-04-05')
     # sb.getNews()
     # sb.fund()
-    sb.get_stock_chengfeng()
+    #sb.get_stock_chengfeng()
+    #sb.date_store()
+    #sb.profit_test()
+    sb.daily_longhu()
 
 
 class Pandas_test:
