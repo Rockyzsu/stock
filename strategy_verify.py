d73ec4c3f6b36031fd354e20bd7201026384d65e
Rockyzsu <jinweizsu@gmail.com>
2017-03-20 02:10:08 +0800

    update

--- strategy_verify.py
+++ strategy_verify.py
@@ -2,6 +2,55 @@
 __author__ = 'Rocky'
 import requests,time,re
 from lxml import etree
+from pandas import DataFrame
+import sqlite3
+# -*-coding=utf-8-*-
+__author__ = 'Rocky'
+import sqlite3
+
+
+
+def create_table():
+    conn = sqlite3.connect('dadanxicou.db')
+    #cur=conn.cursor()
+    try:
+        create_tb_cmd='''
+            CREATE TABLE IF NOT EXISTS HOUSE('日期' TEXT,'代码' TEXT,'股票' TEXT,'买入时间' TEXT,'盈亏' TEXT,'买入价格' TEXT,'当前价格' TEXT,'描述' TEXT);
+            '''
+            #主要就是上面的语句
+            #cur.execute(create_tb_cmd)
+            #cur.commit()
+            #cur.close()
+        conn.execute(create_tb_cmd)
+        conn.commit()
+        conn.close()
+    except:
+        print "Create table failed"
+        return False
+
+
+
+
+def insert( date_time,code,name,trigger_time,profit,trigger_price,current,desc):
+    #def insert(self, code):
+    conn = sqlite3.connect('dadanxicou.db')
+    #cur=conn.cursor()
+    print "open database passed"
+        #conn.text_factory = str
+    cmd="INSERT INTO HOUSE ('日期','代码', '股票','买入时间' ,'盈亏' ,'买入价格' ,'当前价格','描述' ) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s');" %(date_time,code,name,trigger_time,profit,trigger_price,current,desc)
+        #works 要么加\"
+        #paul_su="INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES(5,'%s',32,'CALIFORNIA',2000.00);" %temp2
+        #works 要么加 ’‘
+
+        #allen="INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES(2,'ALLEN',72,'CALIFORNIA',20500.00);"
+        #teddy="INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES(3,'TEDDY',732,'CALIFORNIA',52000.00);"
+        #mark="INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES(4,'MARK',327,'CALIFORNIA',3000.00);"
+        #sun="INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES(?,?,?,?,?);"
+        #conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES(?,?,32,'CALIFORNIA',2000.00)",temp)
+    conn.execute(cmd)
+    conn.commit()
+    conn.close()
+
 class Strategy():
     def __init__(self):
         self.base_url='https://xueqiu.com/strategy/'
@@ -48,40 +97,71 @@ class Strategy():
 
     def getStock(self,strategy,page):
         url='https://xueqiu.com/snowmart/push/stocks.json?product_id=%s&page=%s&count=5' %(str(strategy),str(page))
-        url='https://xueqiu.com/snowmart/push/stocks.json?product_id=19&page=3&count=5'
-        #url='https://xueqiu.com/strategy/'
-        self.headers['Referer']='https://xueqiu.com/strategy/19'
-
+        self.headers['Referer']='https://xueqiu.com/strategy/%s' %str(strategy)
 
         self.headers['X-Requested-With']='XMLHttpRequest'
         self.headers['DNT']='1'
-        #self.headers['Cookie']='s=7e18j5feh8; xq_a_token=720138cf03fb8f84ecc90aab8d619a00dda68f65; xq_r_token=0471237c52a208e16ce2d756fe46219b8066604d; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1489555354; __utma=1.842959324.1489555354.1489555354.1489555354.1; __utmc=1; __utmz=1.1489555354.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); u=301489555354143; Hm_lvt_1db88642e346389874251b5a1eded6e3=1489555354; aliyungf_tc=AQAAAHCnVki7tAwAAzISy6/ldZVhH55k'
-        #need cookies
+        self.headers['Cookie']='s=7e18j5feh8; xq_a_token=720138cf03fb8f84ecc90aab8d619a00dda68f65; xq_r_token=0471237c52a208e16ce2d756fe46219b8066604d; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1489555354; __utma=1.842959324.1489555354.1489555354.1489555354.1; __utmc=1; __utmz=1.1489555354.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); u=301489555354143; Hm_lvt_1db88642e346389874251b5a1eded6e3=1489555354; aliyungf_tc=AQAAAHCnVki7tAwAAzISy6/ldZVhH55k'
         data={'product_id':strategy,'page':page,'count':5}
-
-        #print self.headers
+        resp=requests.get(url,params=data,headers=self.headers)
         return resp.json()
-        s=session.get('http://xueqiu.com',headers=self.headers)
 
-        print self.headers
-        resp=session.get(url,headers=self.headers,params=data).text
-        print resp
+    def dataStore_SQLite(self,strategy,page):
+        json_data=self.getStock(strategy,page)
+        items=json_data['items']
+        #db=DB_store('dadanxicou.db')
+        create_table()
+        for item in items:
+            desc=item['desc'].encode('utf-8')
+            current=item['current']
+            trigger_time=item['trigger_time']
+            name=item['name'].encode('utf-8')
+            trigger_price=item['trigger_price']
+            code=item['symbol'].encode('utf-8')
+            profit=item['change_percent']
+            date_time=item['trigger_time']
+            print type(desc)
+            print type(current)
+            print type(trigger_price)
+            print type(trigger_time)
+            print type(code)
+            print type(profit)
+            print type(date_time)
+            print type(name)
+            #db.insert(date_time,name.decode('utf-8'),code,trigger_time,current,trigger_price,profit,desc.decode('utf-8'))
+            insert(date_time,code,name,trigger_time,profit,trigger_price,current,desc)
 
     def dataFilter(self,strategy,page):
         json_data=self.getStock(strategy,page)
         items=json_data['items']
-        for item in items:
-            print item['name'],
-            print item['trigger_price'],
-            print item['trigger_time'],
-            print item['desc']
+        colums_dict={u'current': '', u'name': '', u'trigger_price': '', u'symbol': '', u'status_id': '', u'trigger_time': '', u'is_new': '', u'change_percent': '', u'flag': '', u'reply_count': '', u'target': '', u'desc': ''}
 
+        df_total=DataFrame(colums_dict,index=['0'])
+        for item in items:
+            #print item
+            df=DataFrame(item,index=['0'])
+            print df
+            df_total=df_total.append(df,ignore_index=True)
+
+            '''
+            current: 目前价格
+            trigger_price: 入选价
+            change_percent: 盈亏比例 小数，非百分比
+            desc: 描述 帖子
+
+            '''
+            #print item['name'],
+            #print item['trigger_price'],
+            #print item['trigger_time'],
+            #print item['desc']
+        #print df_total
+        df_total.to_excel('stragety.xls')
 
 
     def loops(self):
-        for i in range(1,5):
-            self.dataFilter(19,i)
-
+        for i in range(1,10):
+            #self.dataFilter(19,i)
+            self.dataStore_SQLite(19,i)
 '''
 Remote Address:118.178.213.44:443
 Request URL:https://xueqiu.com/snowmart/push/stocks.json?product_id=19&page=3&count=5
