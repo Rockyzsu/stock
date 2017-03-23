7cf30caa3f3eb3c016fb386501e42450eb77675d
Rockyzsu <jinweizsu@gmail.com>
2017-03-23 13:51:29 +0800

    update

--- push_msn.py
+++ push_msn.py
@@ -7,6 +7,7 @@ from toolkit import Toolkit
 from email.mime.multipart import MIMEMultipart
 from email import Encoders, Utils
 from toolkit import Toolkit
+import tushare as ts
 #推送股价信息到手机
 class MailSend():
     def __init__(self, smtp_server, from_mail, password, to_mail):
@@ -18,9 +19,10 @@ class MailSend():
 
         # 初始化邮箱设置
 
-    def send_txt(self, subject,content):
+    def send_txt(self, name,price,percent):
         # 这里发送附件尤其要注意字符编码，当时调试了挺久的，因为收到的文件总是乱码
-
+        content='%s higher than %.2f, %.2f' %(name,price,percent)
+        subject='%s' %name
         self.msg=MIMEText(content,'plain','utf-8')
         self.msg['to'] = self.to_mail
         self.msg['from'] = self.from_mail
@@ -33,13 +35,50 @@ class MailSend():
             self.smtp.login(self.username, self.password)
             self.smtp.sendmail(self.msg['from'], self.msg['to'], self.msg.as_string())
             self.smtp.quit()
+            print "sent"
         except smtplib.SMTPException,e:
             print e
             return 0
 
-def push_msg():
-    cfg=Toolkit('cfg')
+def push_msg(name,price,percent):
+    cfg=Toolkit.getUserData('data.cfg')
     from_mail=cfg['from_mail']
     password=cfg['password']
     to_mail=cfg['to_mail']
     obj=MailSend('smtp.qq.com',from_mail,password,to_mail)
+    obj.send_txt(name,price,percent)
+
+def read_stock():
+    f=open('stock.txt')
+    stock_list=[]
+
+    for s in f.readlines():
+        s= s.strip()
+        row=s.split(';')
+        #print row
+        #print "code :",row[0]
+        #rint "price :",row[1]
+        stock_list.append(row)
+
+    return stock_list
+
+def meet_price(code,price):
+    df=ts.get_realtime_quotes(code)
+    real_price= df['price'].values[0]
+    name=df['name'].values[0]
+    real_price=float(real_price)
+    pre_close=float(df['pre_close'].values[0])
+    percent=(real_price-pre_close)/pre_close*100
+    #percent=df['']
+    #print type(real_price)
+    if real_price>=price:
+        print '%s price higher than %.2f\n'%(name,price)
+        push_msg(name,price,percent)
+
+def main():
+    #read_stock()
+    meet_price('300333',14.1)
+
+
+if __name__=='__main__':
+    main()
