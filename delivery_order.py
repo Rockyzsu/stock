0000767334ac8c9510b7bf57ab02ce16b5eb9f63
Rockyzsu <jinweizsu@gmail.com>
2017-05-05 18:39:12 +0800

    update

--- delivery_order.py
+++ delivery_order.py
@@ -4,11 +4,12 @@ __author__ = 'Rocky'
 import os,datetime
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot  as plt
 pd.set_option('display.max_rows',None)
 class Delivery_Order():
     def __init__(self):
         print "Start"
-        path=os.path.join(os.getcwd(),'data')
+        path=os.path.join(os.getcwd(),'private')
         if os.path.exists(path)==False:
             os.mkdir(path)
         os.chdir(path)
@@ -16,21 +17,25 @@ class Delivery_Order():
     #合并一年的交割单
     def years(self):
         df_list=[]
-        k=[str(i) for i in range(1,5)]
+        k=[str(i) for i in range(1,13)]
         print k
-        for i in range(1,5):
-            filename='2017-%s.xls' %str(i).zfill(2)
+        j=[i for i in range(1,13)]
+        result=[]
+        for i in range(1,13):
+            filename='2016-%s.xls' %str(i).zfill(2)
             #print filename
             t=pd.read_table(filename,encoding='gbk',dtype={u'证券代码':np.str})
             fee=t[u'手续费'].sum()+t[u'印花税'].sum()+t[u'其他杂费'].sum()
             print i," fee: "
             print fee
             df_list.append(t)
-
+            result.append(fee)
         df=pd.concat(df_list,keys=k)
         #print df
         #df.to_excel('2016_delivery_order.xls')
         self.caculation(df)
+        plt.plot(j,result)
+        plt.show()
 
     def caculation(self,df):
         fee=df[u'手续费'].sum()+df[u'印花税'].sum()+df[u'其他杂费'].sum()
