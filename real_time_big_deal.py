154bcac719ff961c0894a9881f8787993f01f3f2
Rockyzsu <jinweizsu@gmail.com>
2017-05-15 15:57:24 +0800

    update

--- real_time_big_deal.py
+++ real_time_big_deal.py
@@ -4,11 +4,12 @@ __author__ = 'Rocky'
 http://30daydo.com
 Contact: weigesysu@qq.com
 '''
-
+import datetime
 import tushare as ts
 import pandas as pd
-import time,os
+import time,os,threading
 import numpy as np
+from toolkit import Toolkit
 pd.set_option('display.max_rows',None)
 class BigMonitor():
     def __init__(self):
@@ -18,7 +19,7 @@ class BigMonitor():
             print "Please put data under data folder"
             exit()
         os.chdir(path)
-
+        self.stockList=Toolkit.read_stock('mystock.csv')
         self.bases=pd.read_csv('bases.csv',dtype={'code':np.str})
 
     def loop(self,code):
@@ -28,17 +29,37 @@ class BigMonitor():
             time.sleep(2)
             df_t1=ts.get_realtime_quotes(code)
             v1=long(df_t1['volume'].values[0])
+            p1=float(df_t1['price'].values[0])
             #print df_t1
             time.sleep(2)
             df_t2=ts.get_realtime_quotes(code)
             v2=long(df_t2['volume'].values[0])
-            delta= v2-v1
-            if delta >10000:
-                print "Big deal"
-                print delta
+            p2=float(df_t2['price'].values[0])
+            delta_v= (v2-v1)/100
+            #换成手
+            #计算价差
+            price_v=p2-p1
+            if delta_v >1000:
+                print datetime.datetime.now().strftime('%H:%M:%S')
+                print "Big deal on %s" %name,
+                print delta_v,'price diff',price_v
+
+
+    def multi_thread(self,code_list):
+        thread_list=[]
+        for i in code_list:
+            t=threading.Thread(target=self.loop,args=(i,))
+            thread_list.append(t)
+
+        for j in thread_list:
+            j.start()
+
+
+    def testcase(self):
+        self.multi_thread(self.stockList)
 
 def main():
     obj=BigMonitor()
-    obj.loop('002868')
+    obj.testcase()
 
 main()
