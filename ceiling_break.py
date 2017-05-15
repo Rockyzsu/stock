440738a3730f153537dccf0f852ee58fb63e4948
Rockyzsu <jinweizsu@gmail.com>
2017-05-15 23:57:03 +0800

    update with thread

--- ceiling_break.py
+++ ceiling_break.py
@@ -1,13 +1,10 @@
-__author__ = 'vmplay'
-#监测开板信号
-
 # -*-coding=utf-8-*-
 __author__ = 'Rocky'
 '''
 http://30daydo.com
 Contact: weigesysu@qq.com
 '''
-import smtplib, time,os,datetime
+import smtplib, time, os, datetime
 from email.mime.text import MIMEText
 from email.header import Header
 from toolkit import Toolkit
@@ -19,6 +16,7 @@ from pandas import Series
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import threading
 
 # 推送股价信息到手机
 class break_monitor():
@@ -27,15 +25,15 @@ class break_monitor():
         from_mail = cfg['from_mail']
         password = cfg['password']
         to_mail = cfg['to_mail']
-        smtp_server='smtp.qq.com'
+        smtp_server = 'smtp.qq.com'
 
         self.server = smtp_server
         self.username = from_mail.split("@")[0]
         self.from_mail = from_mail
         self.password = password
         self.to_mail = to_mail
-        self.bases=pd.read_csv('bases.csv',dtype={'code':np.str})
-        self.stocklist=Toolkit.read_stock('monitor_list.log')
+        self.bases = pd.read_csv('bases.csv', dtype={'code': np.str})
+        self.stocklist = Toolkit.read_stock('monitor_list.log')
         # 初始化邮箱设置读取需要股票信息
         #这样子只登陆一次
         try:
@@ -48,8 +46,6 @@ class break_monitor():
 
     #格式需要修改
     def send_txt(self, name, content):
-
-        content = content + '%'
 
         subject = '%s' % name
         self.msg = MIMEText(content, 'plain', 'utf-8')
@@ -65,53 +61,53 @@ class break_monitor():
             print e
             return 0
 
-
-    def push_msg(self,name, price, percent, status):
-        cfg = Toolkit.getUserData('data.cfg')
-        from_mail = cfg['from_mail']
-        password = cfg['password']
-        to_mail = cfg['to_mail']
-        #obj = MailSend('smtp.qq.com', from_mail, password, to_mail)
-        self.send_txt(name, price, percent, status)
-
-
-
     #开板提示
-    def break_ceil(self,code):
+    def break_ceil(self, code):
+        print "E"
+        print threading.current_thread().name
         while 1:
+            #print code
             time.sleep(2)
             try:
-                df=ts.get_realtime_quotes(code)
+                df = ts.get_realtime_quotes(code)
             except:
                 time.sleep(5)
                 continue
-            v=long(df['b1_v'].values[0])
+            v = long(df['b1_v'].values[0])
             print datetime.datetime.now().strftime("%H:%M:%S")
             print v
             #print type(v)
-            if  v<=10000 :
+            if v <= 10000:
                 print u"小于万手，小心！跑"
 
-                self.push_msg('break',10,10,'down')
+                self.push_msg('break', 10, 10, 'down')
                 #这里可以优化，不必每次都登陆。
 
 
-
     def monitor_break(self):
-        #all_base=pd.read_csv('bases.csv',dtype={'code':np.str})
-        break_ceil('002868')
+        print "C"
+        thread_num = len(self.stocklist)
+        thread_list=[]
+        join_list=[]
+        for i in range(thread_num):
+            print "D"
+            t = threading.Thread(target=self.break_ceil,args=(i,))
+            thread_list.append(t)
+        print "FFFFF"
+        print thread_list
 
-#读入要监测的stock list 多线程运行
-def main():
-    # read_stock()
-    choice = input("Input your choice:\n")
-
+        for j in thread_list:
+            print "GGGGG"
+            j.start()
+            #j.join()
 
 
 if __name__ == '__main__':
-    path=os.path.join(os.getcwd(),'data')
-    if os.path.exists(path)==False:
+    print "A"
+    path = os.path.join(os.getcwd(), 'data')
+    if os.path.exists(path) == False:
         os.mkdir(path)
     os.chdir(path)
-    obj=break_monitor()
+    obj = break_monitor()
+    print 'B'
     obj.monitor_break()
