a88aaa6e8d07b8ff40b3491421606b4a200f79a4
Rock Chen <weigesysu@qq.com>
2017-07-13 18:27:24 +0800

    添加多线程

--- stock_check.py
+++ stock_check.py
@@ -9,10 +9,12 @@ import tushare as ts
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from threading import Thread
 class CheckStock():
     def __init__(self):
         #self.id=id
-        pass
+        self.base = pd.read_csv('bases.csv', dtype={'code': np.str})
+
         '''
         if len(self.id)!=6:
             print "Wrong stock code"
@@ -29,7 +31,7 @@ class CheckStock():
         for i in stock_list:
             i=i.strip()
             ratio_list.append(self.get_info(i))
-        print ratio_list
+        #print ratio_list
         return ratio_list
 
     def get_info(self,id):
@@ -37,40 +39,43 @@ class CheckStock():
         try:
             df=ts.get_today_ticks(id)
             print 'len of df ',len(df)
-            print df
+            #print df
             if len(df)==0:
                 print "Pause of exchange"
-                return
+                return id,'pause'
         except Exception,e:
             print e
             print "ERROR"
+            return id,'pause'
 
+        '''
         print '\n'
         max_p=df['price'].max()
         print max_p
         min_p=df['price'].min()
         print min_p
         #print df
+        '''
         buy= df[df['type']==u'买盘']['volume'].sum()
-        print 'buy:',buy
+        #print 'buy:',buy
         sell =df[df['type']==u'卖盘']['volume'].sum()
-        print 'sell: ',sell
+        #print 'sell: ',sell
         neutral= df[df['type']==u'中性盘']['volume'].sum()
-        print 'neutral: ',neutral
+        #print 'neutral: ',neutral
         #最后一个是开盘数据
         start=df[-1:]
         vol_0=start['volume'].sum()
-        print 'start'
-        print start
+        #print 'start'
+        #print start
         total=buy+sell+neutral+vol_0
 
         sum_all=df['volume'].sum()
 
-        print total
-        print sum_all
+        #print total
+        #print sum_all
 
         ratio=round((buy-sell)*1.00/sell*100,2)
-        print "buy/sell ratio: ",ratio
+        #print "buy/sell ratio: ",ratio
         return id,ratio
         '''
         df['price'].plot()
@@ -81,19 +86,55 @@ class CheckStock():
 
     def show_name(self):
         #self.all=ts.get_stock_basics()
-        self.bases_save=ts.get_stock_basics()
+        #self.bases_save=ts.get_stock_basics()
 
-        self.bases_save.to_csv('bases.csv')
-        self.base = pd.read_csv('bases.csv', dtype={'code': np.str})
+        #self.bases_save.to_csv('bases.csv')
+
         stock_list=self.multi_thread()
         for st in stock_list:
 
             print "code: ",st[0]
-            name=self.base[self.base['code']==st[0]]['name']
+            name=self.base[self.base['code']==st[0]]['name'].values[0]
             print 'name: ',name
             print "ratio: ",st[1]
+            print '\n'
+
+    def sinle_thread(self,start,end):
+        for i in range(start,end):
+            id,ratio=self.get_info(self.all_code[i])
+            if ratio =='pause':
+                continue
+            if ratio>30:
+                print self.base[self.base['code']==id]['name'].values[0],' buy more than 30 percent'
+
+    def scan_all(self):
+        self.all_code=self.base['code'].values
+        thread_num=100
+        all_num=len(self.all_code)
+        each_thread=all_num/thread_num
+        #print type(all_code)
+        thread_list=[]
+        for i in range(thread_num):
+            t=Thread(target=self.sinle_thread,args=(i*each_thread,(i+1)*each_thread))
+            thread_list.append(t)
+
+        for j in thread_list:
+            j.start()
+
+        for k in thread_list:
+            k.join()
+
+
+    def monitor(self):
+        ratio_list=self.multi_thread()
+        for js in ratio_list:
+            if js[1]>30:
+                print js[0]
+
 if __name__=="__main__":
     obj=CheckStock()
     #obj.get_info('000693')
     #obj.multi_thread()
-    obj.show_name()
+    #obj.show_name()
+    #obj.monitor()
+    obj.scan_all()
