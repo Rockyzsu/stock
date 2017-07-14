d1e82373400717ea5b71e91d8873a8cd597b551b
Rock Chen <weigesysu@qq.com>
2017-07-14 18:41:41 +0800

    add multiprocess

--- stock_check.py
+++ stock_check.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from threading import Thread
+from multiprocessing import Pool,Queue,Process,Manager
+import multiprocessing
+multiprocessing.freeze_support()
+
 class CheckStock():
     def __init__(self):
         #self.id=id
@@ -84,6 +88,42 @@ class CheckStock():
         plt.show()
         '''
 
+
+
+    #类中不能多进程
+    def multi_process(self):
+        stock_list=[]
+        with open('stock_list.txt') as f:
+            stock_list=f.readlines()
+        #print stock_list
+        stock_list=map(lambda x:x.strip(),stock_list)
+        #print stock_list
+        '''
+        p=Pool(len(stock_list))
+        result=p.map(self.get_info,stock_list)
+        p.close()
+        p.join()
+        '''
+        p=Pool(len(stock_list))
+        #p_list=[]
+        result=[]
+        for i in stock_list:
+            t=p.apply_async(self.get_info,args=(i,))
+            #p_list.append(Process(target=self.get_info,args=(i)))
+            result.append(t)
+        p.close()
+        p.join()
+        print result
+        '''
+        for j in p_list:
+            j.start()
+        for k in p_list:
+            k.join()
+        '''
+
+
+        print result
+
     def show_name(self):
         #self.all=ts.get_stock_basics()
         #self.bases_save=ts.get_stock_basics()
@@ -97,6 +137,8 @@ class CheckStock():
             name=self.base[self.base['code']==st[0]]['name'].values[0]
             print 'name: ',name
             print "ratio: ",st[1]
+            if st[1]>30:
+                print "WOW, more than 30"
             print '\n'
 
     def sinle_thread(self,start,end):
@@ -109,7 +151,7 @@ class CheckStock():
 
     def scan_all(self):
         self.all_code=self.base['code'].values
-        thread_num=100
+        thread_num=500
         all_num=len(self.all_code)
         each_thread=all_num/thread_num
         #print type(all_code)
@@ -131,10 +173,101 @@ class CheckStock():
             if js[1]>30:
                 print js[0]
 
+
+
+
+def sub_process_ratio(i,q):
+    print "Start"
+    try:
+        df=ts.get_today_ticks(i)
+        #print 'len of df ',len(df)
+        #print df
+        if len(df)==0:
+            print "Pause of exchange"
+            return i,'pause'
+    except Exception,e:
+        print e
+        print "ERROR"
+        return id,'pause'
+
+    '''
+    print '\n'
+    max_p=df['price'].max()
+    print max_p
+    min_p=df['price'].min()
+    print min_p
+    #print df
+    '''
+    buy= df[df['type']==u'买盘']['volume'].sum()
+    #print 'buy:',buy
+    sell =df[df['type']==u'卖盘']['volume'].sum()
+    #print 'sell: ',sell
+    neutral= df[df['type']==u'中性盘']['volume'].sum()
+    #print 'neutral: ',neutral
+    #最后一个是开盘数据
+    start=df[-1:]
+    vol_0=start['volume'].sum()
+    #print 'start'
+    #print start
+    total=buy+sell+neutral+vol_0
+
+    sum_all=df['volume'].sum()
+
+    #print total
+    #print sum_all
+
+    ratio=round((buy-sell)*1.00/sell*100,2)
+    #print "buy/sell ratio: ",ratio
+    s= [i,ratio]
+    print s
+    q.put(s)
+
+def testcase1(i,j,q):
+    print i,j
+    q.put(i)
+
+def multi_process():
+    #stock_list=[]
+    with open('stock_list.txt') as f:
+        stock_list=f.readlines()
+    #print stock_list
+    stock_list=map(lambda x:x.strip(),stock_list)
+    print stock_list
+
+    '''
+    p=Pool(len(stock_list))
+    result=p.map(self.get_info,stock_list)
+    p.close()
+    p.join()
+    '''
+    p=Pool(len(stock_list))
+    #p_list=[]
+    result=[]
+    manager=Manager()
+    q=manager.Queue()
+    #p1=Pool(8)
+    #q1=Queue()
+    for i in stock_list:
+        #print i
+        p.apply_async(sub_process_ratio,args=(i,q))
+        #p1.apply_async(testcase1,args=(i,i,q1))
+        #p_list.append(Process(target=self.get_info,args=(i)))
+        #result.append(t)
+    p.close()
+    p.join()
+    #print result
+
+    while q.empty()==False:
+        print 'get'
+        print q.get()
+
+
 if __name__=="__main__":
-    obj=CheckStock()
+    #obj=CheckStock()
     #obj.get_info('000693')
     #obj.multi_thread()
     #obj.show_name()
     #obj.monitor()
-    obj.scan_all()
+    #obj.scan_all()
+    #obj.multi_process()
+    multi_process()
