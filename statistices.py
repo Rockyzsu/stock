d500f0445fb4593724546f98516536b6189e13e6
[Rocky Chen] <jinweizsu@gmail.com>
2017-07-24 00:47:34 +0800

    np

--- statistices.py
+++ statistices.py
@@ -6,8 +6,13 @@ import alert
 def percentage(sold,buy):
     x=(sold-buy)*1.00/buy*100
     print round(x,2)
+    return x
 
+def buy_price(sold,percent):
+    buy=sold*1.00/(1+percent*1.00/100)
+    print round(buy,2)
+    return buy
 
-
-percentage(0.014,0.0089)
+#percentage(0.196,0.188)
+buy_price(0.196,10)
 
