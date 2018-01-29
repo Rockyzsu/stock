a48d89ba7dd0c013dbdc6ce9f5849d351ce1a7b1
Rockyzsu <jinweizsu@gmail.com>
2018-01-29 23:09:11 +0800

    update letv

--- letv.py
+++ letv.py
@@ -1,17 +1,19 @@
 #-*-coding=utf-8-*-
-import BaseService
 import tushare as ts
-import matplotlib.pyplot as plt
-def main():
-    BaseService.changeDir()
-    df = ts.get_today_ticks('300104')
-    total_vol=df['volume'].sum()
-    big_deal=df[df['volume']>=100]['volume'].sum()
-    percent=float(big_deal)/total_vol*100
-    print percent
-    df['volume'].plot(kind='hist',use_index=False,grid=True)
-    plt.show()
+import os
+cwd=os.getcwd()
+os.chdir(os.path.join(cwd,'data'))
 
+def main():
+    df = ts.get_today_ticks('300104')
+    total_vol=df['volume'].sum()*100
+    print u'总成交股数'
+    big_deal=df[df['volume']>=100]['volume'].sum()*100
+    print u'大于100手的总和',big_deal
+    percent=float(big_deal)/total_vol*100
+    print u'大单占比',percent
+    print df['volume'].value_count()
+    
 
 if __name__ == '__main__':
     main()
