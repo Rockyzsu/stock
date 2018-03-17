f6a88ba0d1bbb32c4fb702415f702d49ee5f0080
Rockyzsu <jinweizsu@gmail.com>
2018-03-17 14:48:30 +0800

    change db name

--- bond_monitor.py
+++ bond_monitor.py
@@ -4,16 +4,16 @@
 '''
 import tushare as ts
 from setting import get_engine
-engine = get_engine('db_bond')
 import pandas as pd
 import datetime
-
+engine = get_engine('db_bond')
 
 class ConvertBond():
 
     def __init__(self):
         self.conn=ts.get_apis()
-        self.allBonds=ts.new_cbonds(pause=2)
+        self.available_bonds=pd.read_sql('tb_bonds_jisilu',engine,index_col='index')
+        self.allBonds=ts.new_cbonds(default=0,pause=2)
         self.onSellBond=self.allBonds.dropna(subset=['marketprice'])
         self.today=datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
@@ -36,7 +36,7 @@ class ConvertBond():
 
 def calculation():
     df=pd.read_sql('bond',engine,index_col='index')
-    df['ration']=df['stock_price']/df['convprice']*100-df['marketprice']
+    df['ration']=(df['stock_price']/df['convprice']*100-df['marketprice'])/(df['stock_price']/df['convprice']*100)
     # print df[df['ration']>0]
     df.to_sql('tb_bond',engine,if_exists='replace')
 def main():
