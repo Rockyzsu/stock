621a978c7cdeb1d3ed78fcd81996235530d6d1d5
Rocky <jinweizsu@gmail.com>
2018-03-25 16:12:13 +0800

    update

--- black_list_sql.py
+++ black_list_sql.py
@@ -2,8 +2,8 @@
 
 import MySQLdb
 import setting
-
-db_name = 'db_stcok'
+import os
+db_name = 'db_stock'
 
 conn = MySQLdb.connect( host = setting.MYSQL_REMOTE,
 						port =3306,
@@ -16,4 +16,47 @@ conn = MySQLdb.connect( host = setting.MYSQL_REMOTE,
 cur = conn.cursor()
 
 def create_tb():
-	cmd = '''CREATE TABLE IF NOT EXISTS `tb_blacklist` ( DATA DATATIME,);'''
+	cmd = '''CREATE TABLE IF NOT EXISTS `tb_blacklist` (DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,CODE VARCHAR(6) PRIMARY KEY,NAME VARCHAR(60),REASON TEXT);'''
+	cur.execute(cmd)
+	conn.commit()
+	# conn.close()
+
+def update_data(filename):
+	with open(filename,'r') as f:
+		content = f.readlines()
+	if not content:
+		exit()
+
+	for line in content:
+		(code,name,reason)=line.strip().split(';')
+
+		cmd='''INSERT INTO `tb_blacklist` (CODE,NAME,REASON) VALUES (\"%s\",\"%s\",\"%s\")''' %(code,name,reason)
+		print cmd
+		try:
+			cur.execute(cmd)
+			conn.commit()
+		except Exception,e:
+			print e
+			conn.rollback()
+from setting import get_engine
+engine=get_engine('db_stock')
+import pandas as pd
+base=pd.read_sql('bases',engine,index_col='index')
+
+def validation():
+	cmd='select `CODE` from tb_blacklist'
+	cur.execute(cmd)
+	result = cur.fetchall()
+	for each in result:
+		# print each[0]
+		print base[base['code']==each[0]]['name'].values[0],'\t',
+		print base[base['code']==each[0]]['area'].values[0]
+
+def main():
+	# filename=os.path.join(os.path.dirname(__file__),'data','blacklist.csv')
+	# create_tb()
+	# update_data(filename)
+	validation()
+
+if __name__=='__main__':
+	main()
