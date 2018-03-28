55c953a2cc65c9651a43ca4c612689e51f3dc43c
Rockyzsu <jinweizsu@gmail.com>
2018-03-28 09:09:47 +0800

    remove print

--- jisilu.py
+++ jisilu.py
@@ -78,7 +78,6 @@ class Jisilu():
                        'put_convert_price':u'回售 触发价','short_maturity_dt':u'到期时间','volume':u'成交额(万元)','price':u'可转债价格'})
             df=df[[u'可转债代码',u'可转债名称',u'可转债涨幅',u'可转债价格',u'正股名称',u'正股现价',u'正股涨跌幅',u'最新转股价',u'溢价率',u'回售 触发价',u'到期时间']]
             df[u'更新日期']=datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        print df
         df.to_sql('tb_bond_jisilu',engine,if_exists='replace')
 
 def main():
