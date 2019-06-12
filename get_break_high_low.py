ca12c053db2de44ca4ea94d70e044e77c81cf1a9
Rocky Chen <jinweizsu@gmail.com>
2019-06-12 11:11:34 +0800

    创新高与创新低，更新

--- get_break_high_low.py
+++ get_break_high_low.py
@@ -1,4 +1,6 @@
 # -*-coding=utf-8-*-
+import time
+
 __author__ = 'rocky'
 # 获取破指定天数内的新高 比如破60日新高
 import tushare as ts
@@ -6,6 +8,7 @@ import datetime
 import pandas as pd
 from setting import get_engine, get_mysql_conn
 import pymongo
+from config import token
 
 MONGO_HOST = '10.18.6.46'
 MONGO_PORT = 27001
@@ -19,33 +22,49 @@ class BreakPoin(object):
         self.info = pd.read_sql('tb_basic_info', con=self.engine, index_col='code')
         self.db = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
         self.doc = self.db['db_stock']['break_low_high']
+        ts.set_token(token)
+        self.pro = ts.pro_api()
+        self.count = 0
 
-    def loop_stocks(self):
-
+    def loop_stocks(self,day):
+        total = 200+10
+        each_loop = 60/total
         for idx, row in self.info.iterrows():
             stock_code = idx
-            self.is_break(stock_code, 90, break_type='high', stock_type='stock')
-            self.is_break(stock_code, 90, break_type='low', stock_type='stock')
+            print('Checking {}'.format(stock_code))
+            self.is_break(stock_code, day, stock_type='stock')
+            time.sleep(each_loop)
 
-    def is_break(self, stockID, days, break_type, stock_type):
+    def code_convert(self, code):
+        if code[0] == '6':
+            return code + '.SH'
+        else:
+            return code + '.SZ'
+
+    def is_break(self, stockID, day, stock_type):
 
         end_day = datetime.datetime.now()
-        days = days * 7 / 5
+        days = day * 7 / 5
         # 考虑到周六日非交易
         start_day = end_day - datetime.timedelta(days)
 
-        start_day = start_day.strftime("%Y-%m-%d")
-        end_day = end_day.strftime("%Y-%m-%d")
+        start_day = start_day.strftime("%Y%m%d")
+        end_day = end_day.strftime("%Y%m%d")
         name = self.info.ix[stockID]['name']
         # get_h_data 有问题。
 
         try:
 
-            df = ts.get_k_data(stockID, start=start_day, end=end_day)
+            # df = ts.get_k_data(stockID, start=start_day, end=end_day)
+            ts_code = self.code_convert(stockID)
+            df = ts.pro_bar(ts_code=ts_code, adj='qfq', start_date=start_day, end_date=end_day)
+            # df 第0个数据是最新的。
 
         except Exception as e:
             print(e)
             print('{} {}获取行情识别'.format(stockID, name))
+            time.sleep(30)
+
             return False
 
         if df is None or df.empty:
@@ -56,53 +75,35 @@ class BreakPoin(object):
             print('上市时间太短'.format(stockID, name))
             return False
 
-        if break_type == 'high':
+        period_high = df['close'][1:].max()
+        today_high = df.iloc[0]['high']
 
-            period_high = df['high'][:-1].max()
-            today_high = df.iloc[-1]['high']
-            # 这里不能直接用 .values
-            # 如果用的df【：1】 就需要用.values
-            # print(today_high)
+        if today_high >= period_high:
+            stock_h = []
 
-            if today_high >= period_high:
+            stock_h.append(stockID)
+            stock_h.append(name)
+            insert_dict = {'类型': '新高', '范围': days, '名称': name, '代码': stockID, 'run_time': datetime.datetime.now(),
+                           '品种': stock_type,'开始日期':start_day,'结束日期':end_day}
+            self.doc.insert_one(insert_dict)
 
-                stock_h = []
+        period_low = df['close'][1:].min()
+        today_low = df.iloc[0]['low']
 
-                stock_h.append(stockID)
-                stock_h.append(name)
-                insert_dict = {'类型': '新高', '范围': days, '名称': name, '代码': stockID, 'date': datetime.datetime.now(),
-                               '品种': stock_type}
-                self.doc.insert_one(insert_dict)
-                return True
+        if today_low <= period_low:
+            stock_l = []
 
-            else:
-
-                return False
-
-        elif break_type == 'low':
-
-            period_low = df['low'][:-1].min()
-            today_low = df.iloc[-1]['low']
-
-            if today_low <= period_low:
-
-                stock_l = []
-
-                name = self.info.ix[stockID]['name']
-                stock_l.append(stockID)
-                stock_l.append(name)
-                print('新低', stock_l)
-                insert_dict = {'类型': '新低', '范围': days, '名称': name, '代码': stockID, 'date': datetime.datetime.now(),
-                               '品种': stock_type}
-                self.doc.insert_one(insert_dict)
-
-                return True
-
-            else:
-                return False
+            name = self.info.ix[stockID]['name']
+            stock_l.append(stockID)
+            stock_l.append(name)
+            print('新低', stock_l)
+            insert_dict = {'类型': '新低', '范围': days, '名称': name, '代码': stockID, 'date': datetime.datetime.now(),
+                           '品种': stock_type}
+            self.doc.insert_one(insert_dict)
 
 
 if __name__ == '__main__':
     obj = BreakPoin()
-    obj.loop_stocks()
+    cal_day = 90
+    obj.loop_stocks(cal_day)
     print("Done")
