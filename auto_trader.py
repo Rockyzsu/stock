f86652485e472f433eb92a20d31b9dbe60388935
Rocky Chen <weigesysu@qq.com>
2020-11-14 16:02:14 +0800

    update ubuntu

--- auto_trader.py
+++ auto_trader.py
@@ -9,10 +9,13 @@ import pymongo
 import easyquotation
 import easytrader
 import pandas as pd
-from config import PROGRAM_PATH,MONGO_PORT,MONGO_HOST
-from settings import get_engine,get_mysql_conn
+from config import PROGRAM_PATH, MONGO_PORT, MONGO_HOST
+from settings import DBSelector
 
-SELL = 7 # 配置为8%个点卖
+SELL = 7  # 配置为8%个点卖
+
+DB = DBSelector()
+
 
 class AutoTrader():
 
@@ -30,12 +33,13 @@ class AutoTrader():
         # self.blacklist_bond = self.get_blacklist()
         # self.q=easyquotation.use('qq')
 
-        self.yesterday = datetime.datetime.now()+datetime.timedelta(days=-1)
+        self.yesterday = datetime.datetime.now() + datetime.timedelta(days=-1)
         # 如果是周一 加一个判断
-        self.yesterday=self.yesterday.strftime('%Y-%m-%d')
+        self.yesterday = self.yesterday.strftime('%Y-%m-%d')
 
     def get_close_price(self):
-        conn = get_mysql_conn('db_jisilu','local')
+
+        conn = DB.get_mysql_conn('db_jisilu', 'qq')
         cursor = conn.cursor()
 
         cmd = 'select 可转债代码,可转债价格 from `tb_jsl_{}`'.format(self.yesterday)
@@ -46,39 +50,36 @@ class AutoTrader():
             return None
 
         else:
-            d={}
+            d = {}
             for item in result:
-                d[item[0]]=item[1]
+                d[item[0]] = item[1]
             return d
-
 
     # 设置涨停 附近卖出 挂单
     def set_ceiling(self):
-        position =self.get_position()
+        position = self.get_position()
         # print(position)
         code_price = self.get_close_price()
 
         for each_stock in position:
             try:
-                code=each_stock.get('证券代码')
-                amount = int(each_stock.get('可用余额',0))
-                if amount<=0.1:
+                code = each_stock.get('证券代码')
+                amount = int(each_stock.get('可用余额', 0))
+                if amount <= 0.1:
                     continue
-                close_price = code_price.get(code,None)
-                buy_price = round(close_price * (1+SELL*0.01),1)
+                close_price = code_price.get(code, None)
+                buy_price = round(close_price * (1 + SELL * 0.01), 1)
                 self.user.sell(code, price=buy_price, amount=amount)
 
             except Exception as e:
 
                 self.logger.error(e)
 
-
-
     # 获取候选股票池数据
     def get_candidates(self):
         stock_candidate_df = pd.read_sql(
             'tb_stock_candidates', con=self.engine)
-        stock_candidate_df=stock_candidate_df.sort_values(by='可转债价格')
+        stock_candidate_df = stock_candidate_df.sort_values(by='可转债价格')
         return stock_candidate_df
 
     def get_market_data(self):
@@ -91,32 +92,32 @@ class AutoTrader():
         return black_list_df['code'].values
 
     # 开盘前统一下单
-    def morning_start(self,p):
+    def morning_start(self, p):
         # print(self.user.balance)
         codes = self.stock_candidates['可转债代码']
         prices = self.stock_candidates['可转债价格']
         code_price_dict = dict(zip(codes, prices))
-        count=0
+        count = 0
         while 1:
-            count+=1
+            count += 1
             logging.info('Looping {}'.format(count))
-            
+
             for code, price in code_price_dict.copy().items():
                 # 价格设定为昨天收盘价的-2%
                 if code not in self.blacklist_bond:
                     # buy_price=round(price*0.98,2)
                     deal_detail = self.q.stocks(code)
-                    close=deal_detail.get(code,{}).get('close') # 昨日收盘
-                    ask=deal_detail.get(code,{}).get('ask1') # 卖一
-                    bid=deal_detail.get(code,{}).get('bid1') # 买一价
-                    current_percent = (ask-close)/close*100
+                    close = deal_detail.get(code, {}).get('close')  # 昨日收盘
+                    ask = deal_detail.get(code, {}).get('ask1')  # 卖一
+                    bid = deal_detail.get(code, {}).get('bid1')  # 买一价
+                    current_percent = (ask - close) / close * 100
                     # print(current_percent)
                     if current_percent <= p:
-                        self.logger.info('>>>>代码{}, 当前价格{}, 开盘跌幅{}'.format(code,bid,current_percent))
+                        self.logger.info('>>>>代码{}, 当前价格{}, 开盘跌幅{}'.format(code, bid, current_percent))
 
                         try:
-                            print('code {} buy price {}'.format(code,ask))
-                            self.user.buy(code,price=ask+0.1,amount=10)
+                            print('code {} buy price {}'.format(code, ask))
+                            self.user.buy(code, price=ask + 0.1, amount=10)
                         except Exception as e:
                             self.logger.error('>>>>买入{}出错'.format(code))
                             self.logger.error(e)
@@ -148,15 +149,13 @@ class AutoTrader():
 
     def save_position(self):
 
-        self.engine = get_engine('db_position', True)
-        df= self.get_position_df()
+        self.engine = DB.get_engine('db_position', 'qq')
+        df = self.get_position_df()
         # print(df)
         try:
-            df.to_sql('tb_position_{}'.format(self.today),con=self.engine,if_exists='replace')
+            df.to_sql('tb_position_{}'.format(self.today), con=self.engine, if_exists='replace')
         except Exception as e:
             self.logger.error(e)
-
-
 
     def llogger(self, filename):
         logger = logging.getLogger(filename)  # 不加名称设置root logger
@@ -181,7 +180,6 @@ class AutoTrader():
         self.logger.info('程序退出')
 
 
-
 if __name__ == '__main__':
     trader = AutoTrader()
     # 开盘挂单
@@ -190,4 +188,3 @@ if __name__ == '__main__':
     # trader.save_position()
     # trader.end()
     trader.set_ceiling()
-
---
+++ source_code_reading/source_code.py
@@ -0,0 +1,8 @@
+import scrapy as sc
+import easyquotation as ea
+print(ea.__version__)
+print(ea.__author__)
+print(sc.__version__)
+# print(sc.__author__)
+
+
