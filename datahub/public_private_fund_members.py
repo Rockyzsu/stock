f384f58dc550381442fa398a0eaecba57619f60b
Rockyzsu <jinweizsu@gmail.com>
2021-03-16 17:32:15 +0800

    fix

--- datahub/public_private_fund_members.py
+++ datahub/public_private_fund_members.py
@@ -1,4 +1,6 @@
 # 公募私募人员数据获取
+import sys
+
 sys.path.append('..')
 from configure.settings import DBSelector, send_from_aliyun
 from common.BaseService import BaseService
@@ -13,42 +15,31 @@ from sqlalchemy import Column, String, INTEGER, VARCHAR, DATE, DateTime, Foreign
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class FundMembers(BaseService):
 
-    def __init__(self, kind,date,first_use=False):
+    def __init__(self, kind, date, first_use=False):
         super(FundMembers, self).__init__(first_use)
 
-        self.headers = {
-            "Host": "query.sse.com.cn",
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0",
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate",
-            "Connection": "keep-alive",
-            "Referer": "http://www.sse.com.cn/market/funddata/volumn/lofvolumn/",
-        }
         self.lof_url = 'http://query.sse.com.cn/commonQuery.do?=&jsonCallBack=jsonpCallback1681&sqlId=COMMON_SSE_FUND_LOF_SCALE_CX_S&pageHelp.pageSize=10000&FILEDATE={}&_=161146986468'
         self.etf_url = 'http://query.sse.com.cn/commonQuery.do?jsonCallBack=jsonpCallback28550&isPagination=true&pageHelp.pageSize=25&pageHelp.pageNo={}&pageHelp.cacheSize=1&sqlId=COMMON_SSE_ZQPZ_ETFZL_XXPL_ETFGM_SEARCH_L&STAT_DATE={}&pageHelp.beginPage={}&pageHelp.endPage=30&_=1611473902414'
 
         self.db = DBSelector()
 
-
-
-
         # self.today ='2021-01-22' # ETF
-
+        self.today_='' # TODO failed
         self.ETF_COUNT_PER_PAGE = 25
         self.url_option_dict = {
             'ETF': {'url': self.etf_url, 'date': self.today},
             'LOF': {'url': self.lof_url, 'date': self.today_}
         }
 
-        self.kind=kind.lower()
+        self.kind = kind.lower()
         self.session = requests.Session()
         self.logger.info('start...sh fund')
         self.LAST_TEXT = ''
 
-        if self.first_use:
+        if first_use:
             self.create_table()
 
         self.db_session = self.get_session()
@@ -62,6 +53,18 @@ class FundMembers(BaseService):
         js_data = self.jsonp2json(content)
         self.process_lof(js_data)
 
+    @property
+    def headers(self):
+        return {
+            "Host": "query.sse.com.cn",
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "keep-alive",
+            "Referer": "http://www.sse.com.cn/market/funddata/volumn/lofvolumn/",
+        }
+
     def process_lof(self, js_data):
         result = js_data.get('result')
         for item in result:
@@ -70,14 +73,14 @@ class FundMembers(BaseService):
             date = item['TRADE_DATE']
 
             try:
-                share = float(item['INTERNAL_VOL'].replace(',',''))
+                share = float(item['INTERNAL_VOL'].replace(',', ''))
             except Exception as e:
                 print(e)
-                share=None
+                share = None
 
             self.process_model(code, name, date, share, 'LOF')
 
-    def post(self,url, post_data, _josn=False, binary=False, retry=5):
+    def post(self, url, post_data, _josn=False, binary=False, retry=5):
 
     def crawl_etf(self):
         options = self.url_option_dict['ETF']
@@ -115,10 +118,10 @@ class FundMembers(BaseService):
     def run(self):
         'LOF 与 ETF'
         # for type_, options in self.url_option_dict.items():
-        if self.kind=='etf':
+        if self.kind == 'etf':
             self.logger.info('crawling etf .....')
             self.crawl_etf()
-        if self.kind=='lof':
+        if self.kind == 'lof':
             self.logger.info('crawling lof .....')
             self.crawl_lof()
 
