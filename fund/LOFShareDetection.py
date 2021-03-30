474089ec7b16a7bec1f3fa4cdd77737670dc5241
Rockyzsu <jinweizsu@gmail.com>
2021-03-30 14:31:43 +0800

    change link

---
+++ fund/LOFShareDetection.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/3/29 22:33
+# @File : LOFShareDetection.py
+# @Author : Rocky C@www.30daydo.com
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
---
+++ fund/LOF_Model.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/3/29 22:30
+# @File : LOF_Model.py
+# @Author : Rocky C@www.30daydo.com
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
