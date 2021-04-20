8615650fe077d19f14c995dcdd2f271582842e57
Rockyzsu <jinweizsu@gmail.com>
2021-04-20 08:42:54 +0800

    share monitor

--- fund/LOF_Model.py
+++ fund/LOF_Model.py
@@ -2,10 +2,46 @@
 # @Time : 2021/3/29 22:30
 # @File : LOF_Model.py
 # @Author : Rocky C@www.30daydo.com
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, INTEGER, VARCHAR, DATE, DateTime, ForeignKey, FLOAT
+
+# 创建对象的基类:
+Base = declarative_base()
 
 
-def main():
-    pass
+class FundBaseInfoModel(Base):
+    # 基本表
+    # 表的名字:
+    __tablename__ = 'LOF_BaseInfo'
 
-if __name__=='__main__':
-    main()
+    # 表的结构:
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    code = Column(VARCHAR(6), comment='基金代码', unique=True)
+    name = Column(VARCHAR(40), comment='基金名称')
+    category = Column(VARCHAR(8), comment='基金类别')
+    invest_type = Column(VARCHAR(6), comment='投资类别')
+    manager_name = Column(VARCHAR(48), comment='管理人呢名称')
+    issue_date = Column(DATE, comment='上市日期')
+    # child = relationship('ShareModel', back_populates='LOF_BaseInfo')
+    child = relationship('ShareModel')
+
+    def __str__(self):
+        return f'<{self.code}><{self.name}>'
+
+
+class ShareModel(Base):
+    # 详情表 不只是LOF，ETF，封基也被包含了
+    # 表的名字:
+    __tablename__ = 'LOF_Share'
+
+    # 表的结构:
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    code = Column(VARCHAR(6), ForeignKey('LOF_BaseInfo.code'), comment='代码')
+    date = Column(DATE, comment='份额日期')
+    share = Column(FLOAT, comment='份额 单位：万份')
+    parent = relationship('FundBaseInfoModel')
+    # parent = relationship('FundBaseInfoModel', back_populates='LOF_Share')
+    crawltime = Column(DateTime, comment='爬取日期')
+
--- fund/fund_share_crawl.py
+++ fund/fund_share_crawl.py
@@ -16,51 +16,10 @@ from common.BaseService import BaseService
 import requests
 import warnings
 import datetime
-
+from fund.LOF_Model import Base,FundBaseInfoModel,ShareModel
 warnings.filterwarnings("ignore")
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, INTEGER, VARCHAR, DATE, DateTime, ForeignKey, FLOAT
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-# 创建对象的基类:
-Base = declarative_base()
-
-
-class FundBaseInfoModel(Base):
-    # 基本表
-    # 表的名字:
-    __tablename__ = 'LOF_BaseInfo'
-
-    # 表的结构:
-    id = Column(INTEGER, primary_key=True, autoincrement=True)
-    code = Column(VARCHAR(6), comment='基金代码', unique=True)
-    name = Column(VARCHAR(40), comment='基金名称')
-    category = Column(VARCHAR(8), comment='基金类别')
-    invest_type = Column(VARCHAR(6), comment='投资类别')
-    manager_name = Column(VARCHAR(48), comment='管理人呢名称')
-    issue_date = Column(DATE, comment='上市日期')
-    # child = relationship('ShareModel', back_populates='LOF_BaseInfo')
-    child = relationship('ShareModel')
-
-    def __str__(self):
-        return f'<{self.code}><{self.name}>'
-
-
-class ShareModel(Base):
-    # 详情表 不只是LOF，ETF，封基也被包含了
-    # 表的名字:
-    __tablename__ = 'LOF_Share'
-
-    # 表的结构:
-
-    id = Column(INTEGER, primary_key=True, autoincrement=True)
-    code = Column(VARCHAR(6), ForeignKey('LOF_BaseInfo.code'), comment='代码')
-    date = Column(DATE, comment='份额日期')
-    share = Column(FLOAT, comment='份额 单位：万份')
-    parent = relationship('FundBaseInfoModel')
-    # parent = relationship('FundBaseInfoModel', back_populates='LOF_Share')
-    crawltime = Column(DateTime, comment='爬取日期')
 
 
 class Fund(BaseService):
@@ -226,7 +185,7 @@ class SZFundShare(Fund):
         page = 1
         self.stop = False
         while not self.stop:
-            content = self.get(self.all_fund_url.format(page))
+            content = self.get(self.all_fund_url.format(page),_json=True)
             for item in self.json_parse(content):
                 self.model_process(*item)
 
