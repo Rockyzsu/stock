1fc9378d6910a8dfbf7619255b6e2762767edf09
Rocky Chen <jinweizsu@gmail.com>
2025-04-21 15:46:14 +0800

    init

--- fund/LOF_arbitrage.py
+++ fund/LOF_arbitrage.py
@@ -5,34 +5,38 @@ import demjson
 import fire
 import pandas as pd
 import requests
-import  sys
+import sys
+
 sys.path.append('..')
 from configure.settings import DBSelector
 from configure.util import send_message_via_wechat
 import akshare as ak
 from parsel import Selector
 
+TARGET_PREMIUM = 4  # 溢价率阈值
+
 
 class LOF_arbitrage:
-    def __init__(self,save):
+    def __init__(self, save):
         self.db = DBSelector().mongo()
         self.client = self.db['FUND_LOF']
         self.save = save
 
     def update_premiun(self):
         date = datetime.datetime.now().strftime('%Y-%m-%d')
-        doc =self.client[date]
+        doc = self.client[date]
         for fund in doc.find():
             price = float(fund['trade'])
             try:
                 netvalue = float(fund['单位净值'])
-                discount = (price-netvalue) / netvalue * 100
-                doc.update_one({'symbol':fund['symbol']},{'$set':{"溢价率":discount}})
+                discount = (price - netvalue) / netvalue * 100
+                doc.update_one({'symbol': fund['symbol']}, {'$set': {"溢价率": discount}})
             except Exception as e:
                 print(e)
-                print(fund['symbol'],'error')
+                print(fund['symbol'], 'error')
+
     def get_realtime_time(self):
-        result =[]
+        result = []
         for p in range(1, 11):
             fund_list = self.get_page(p)
             for fund in fund_list:
@@ -44,20 +48,20 @@ class LOF_arbitrage:
                 fund.update(detail_dict)
                 fund['update_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 try:
-                    price=float(fund['trade'])
+                    price = float(fund['trade'])
                     netvalue = float(fund['单位净值'])
-                    discount = (price-netvalue) / netvalue * 100
+                    discount = (price - netvalue) / netvalue * 100
                     fund['溢价率'] = discount
                     # if self.save:
                     #     self.dump_mongodb(fund)
                     result.append(fund)
                 except Exception as e:
                     print(e)
-                    print(fund['symbol'],'error')
+                    print(fund['symbol'], 'error')
 
         return result
 
-    def dump_mongodb(self,data):
+    def dump_mongodb(self, data):
         doc = datetime.datetime.now().strftime('%Y-%m-%d')
         self.client[doc].insert_many(data)
 
@@ -68,7 +72,8 @@ class LOF_arbitrage:
         #     except Exception as e:
         #         print(e)
         #         print(d)
-    def fund_detail(self,code):
+
+    def fund_detail(self, code):
 
         url = "https://finance.sina.com.cn/fund/quotes/{}/bc.shtml".format(code)
 
@@ -87,15 +92,15 @@ class LOF_arbitrage:
 
         response = requests.request("GET", url, headers=headers, data=payload)
         if '<meta charset="utf-8"/>' in response.text:
-            response.encoding='utf-8'
+            response.encoding = 'utf-8'
         else:
-            response.encoding='gb2312'
+            response.encoding = 'gb2312'
 
         result = self.parse(response.text)
         return result
 
-    def parse(self,content):
-        xpath_data =Selector(text=content)
+    def parse(self, content):
+        xpath_data = Selector(text=content)
 
         result = {}
         nodes = xpath_data.xpath('//div[@class="fund_data_item"]')
@@ -105,30 +110,37 @@ class LOF_arbitrage:
             if value is None:
                 value = node.xpath('./span[@class="fund_data" or @class="fund_data_green"]/text()').extract_first()
 
-            value = value.replace('%','').replace('/','')
+            value = value.replace('%', '').replace('/', '')
             result[name] = value
 
-        if len(result)==0:
+        if len(result) == 0:
             # 额外解析
             th_node = xpath_data.xpath('//div[@id="fund-hq"]//table//th')
             for node in th_node:
                 name = node.xpath('./text()').extract_first()
                 value = node.xpath('./span/text()').extract_first()
-                name = name.replace('：','').replace(':','')
+                name = name.replace('：', '').replace(':', '')
                 # if len(td_node)>0:
                 #     value = td_node[0].xpath('./text()').extract_first()
                 #     result[name] = value
                 value = value.replace('%', '')
                 # print(name,value)
-                if name == '基金简称' or name =='申购状态':
+                if name == '基金简称' or name == '申购状态':
                     continue
                 result[name] = value
 
         return result
 
-    def get_page(self,p):
+    def postfix(self, code):
+        if code.starts('5'):
+            return 'SH' + code
+        else:
+            return 'SZ' + code
 
-        url = "https://vip.stock.finance.sina.com.cn/quotes_service/api/jsonp.php/IO.XSRV2.CallbackList['BwSgm6CTKGGEoOxP']/Market_Center.getHQNodeDataSimple?page={}&num=40&sort=symbol&asc=1&node=lof_hq_fund&%5Bobject%20HTMLDivElement%5D=etp68".format(p)
+    def get_page(self, p):
+
+        url = "https://vip.stock.finance.sina.com.cn/quotes_service/api/jsonp.php/IO.XSRV2.CallbackList['BwSgm6CTKGGEoOxP']/Market_Center.getHQNodeDataSimple?page={}&num=40&sort=symbol&asc=1&node=lof_hq_fund&%5Bobject%20HTMLDivElement%5D=etp68".format(
+            p)
         payload = {}
         headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
@@ -136,7 +148,7 @@ class LOF_arbitrage:
         data = response.text
         start = data.index('[{')
         end = data.rindex(']')
-        data = data[start:end+1]
+        data = data[start:end + 1]
         py_object = demjson.decode(data)
         return py_object
 
@@ -144,30 +156,40 @@ class LOF_arbitrage:
         result = self.get_realtime_time()
         df = pd.DataFrame(result)
         fund_purchase_em_df = ak.fund_purchase_em()
-        merge_df = pd.merge(df,fund_purchase_em_df,left_on='code',right_on='基金代码',how='left')
+        merge_df = pd.merge(df, fund_purchase_em_df, left_on='code', right_on='基金代码', how='left')
         if self.save:
             merge_df['下一开放日'] = merge_df['下一开放日'].map(lambda x: str(x))
             # merge_df['下一开放日'] = merge_df['下一开放日'].fillna('')
-            merge_df['最新净值/万份收益-报告时间']=merge_df['最新净值/万份收益-报告时间'].map(lambda x:str(x))
+            merge_df['最新净值/万份收益-报告时间'] = merge_df['最新净值/万份收益-报告时间'].map(lambda x: str(x))
             json_data = merge_df.to_dict(orient='records')
             try:
                 self.dump_mongodb(json_data)
             except Exception as e:
                 send_message_via_wechat('LOF溢价率数据存储失败')
+        target_df = merge_df[merge_df['溢价率'] >= TARGET_PREMIUM]
 
-        target_df = merge_df[merge_df['溢价率']>=5]
-
-        for index,row in target_df.iterrows():
+        for index, row in target_df.iterrows():
             code = row['code']
             name = row['基金简称']
-            premium = round(float(row['溢价率']),2)
+            premium = round(float(row['溢价率']), 2)
             status = row['申购状态']
             limit_amount = row['日累计限定金额']
-            if status=='暂停申购':
+            if status == '暂停申购':
                 continue
+            _code = self.postfix(code)
 
-            msg = '{} {} 溢价率 {},{},{}'.format(code,name,premium,status,limit_amount)
-            send_message_via_wechat(msg)
+            try:
+                stock_individual_spot_xq_df = ak.stock_individual_spot_xq(symbol=_code)
+                volume = stock_individual_spot_xq_df[stock_individual_spot_xq_df['item'] == '成交额']['value'].iloc[0]
+            except Exception as e:
+                msg = '{} {} 溢价率 {},{},{}'.format(code, name, premium, status, limit_amount)
+                send_message_via_wechat(msg)
+
+            else:
+                if volume / 10000 >= 10:  # 大于10万成交额
+                    msg = '{} {} 溢价率 {},{},{}，成交量{}'.format(code, name, premium, status, limit_amount, volume)
+                    send_message_via_wechat(msg)
+
 
 def main(save=False):
     today = datetime.datetime.now()
@@ -178,6 +200,7 @@ def main(save=False):
 
     app = LOF_arbitrage(save)
     app.run()
+
 
 if __name__ == '__main__':
     fire.Fire(main)
